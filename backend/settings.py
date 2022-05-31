ab33874ed194e1819a2abaf0c2536010aa374ef3
bocchianaba <45669998+bocchianaba@users.noreply.github.com>
2022-05-31 11:31:08 +0100

    refix the db

--- backend/settings.py
+++ backend/settings.py
@@ -117,7 +117,7 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',#'django.db.backends.postgresql_psycopg2',
-        'NAME': 'db.sqlite3', 
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'), 
         # 'USER': 'postgres', 
         # 'PASSWORD': 'postgres',
         # 'HOST': '127.0.0.1', 
